3c808849cd302917fd8709c8d40f5356a36215e2
Stefan Meyre <stefan@hb9gze.ch>
2024-12-09 09:52:21 +0100

    Initial Commit

---
+++ Code/PhasorRadio1/STM32CubeIDE/Debug/Application/User/TouchGFX/App/app_touchgfx.d
@@ -0,0 +1,4 @@
+Application/User/TouchGFX/App/app_touchgfx.o: \
+ C:/ProjectsOnC/Version_UART/PhasorRadio/Code/PhasorRadio1/TouchGFX/App/app_touchgfx.c \
+ C:/ProjectsOnC/Version_UART/PhasorRadio/Code/PhasorRadio1/TouchGFX/App/app_touchgfx.h
+C:/ProjectsOnC/Version_UART/PhasorRadio/Code/PhasorRadio1/TouchGFX/App/app_touchgfx.h:
---
+++ Code/PhasorRadio1/STM32CubeIDE/Debug/Drivers/BSP/Components/ft5336_reg.d
@@ -0,0 +1,4 @@
+Drivers/BSP/Components/ft5336_reg.o: \
+ C:/ProjectsOnC/Version_UART/PhasorRadio/Code/PhasorRadio1/Drivers/BSP/Components/ft5336/ft5336_reg.c \
+ C:/ProjectsOnC/Version_UART/PhasorRadio/Code/PhasorRadio1/Drivers/BSP/Components/ft5336/ft5336_reg.h
+C:/ProjectsOnC/Version_UART/PhasorRadio/Code/PhasorRadio1/Drivers/BSP/Components/ft5336/ft5336_reg.h:
